8ff2b4cd08e6fda4f3a516f1cdf5924f592cc224
Griffin Shelor <griffinshelor@gmail.com>
2024-01-23 11:37:50 -0800

    Initial commit

---
+++ Labs/DDPopulationModel.R
@@ -0,0 +1,29 @@
+## Lab 1 DD population model
+## Griffin Shelor
+
+## assigning values to r and K, creating time vector, creating N vector
+r <- 0.4
+K <- 250
+time_vector <- seq(1:100)
+N <- rep(0, 100)
+N[1] <- 3
+
+## for loop
+for (i in 2:length(time_vector)) {
+  N[i] = N[i - 1] + r * N[i - 1] * (1 - N[i - 1]/K)
+}
+
+## plotting population over time
+plot(x = time_vector, y = N)
+
+
+## trying a function
+logistic <- function(n, R, k) {
+  new_n <- n + R * n * (1 - n / k)
+  return(new_n)
+}
+
+for (i in 2:length(time_vector)) {
+  N[i] <- logistic(n = N[i - 1], R = r, k = K)
+}
+plot(x = time_vector, y = N, main = "Functionalized Model")
